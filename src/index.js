e4b00fa43fcd79d4f2cf828dee904baffd94807c
Gregory Kravchenko <2crazymail@gmail.com>
2019-10-15 23:05:44 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -9,6 +9,15 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+    let factor = config[focus] || 0,
+        full_time = study_time(knowsProgramming);
+
+    return factor == 0 ? full_time : Math.ceil(full_time / factor);
   };
-  
+
+function study_time(knowsProgramming) {
+    let weeks_with_basic = 800,
+        weeks_without_basic = weeks_with_basic + 500;
+
+    return knowsProgramming ? weeks_with_basic : weeks_without_basic;
+}
